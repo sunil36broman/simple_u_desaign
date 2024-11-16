37cf05a564779fef5dc5907a4ac144680dc46f0f
sunil_broman <sunil.chandra@upaybd.com>
2024-11-16 22:58:29 +0600

    init

--- app/categories/[categoryId]/page.tsx
+++ app/categories/[categoryId]/page.tsx
@@ -9,34 +9,11 @@ interface CategoryDetailsPageProps {
 export default async function CategoryDetailsPage({ params }: CategoryDetailsPageProps) {
   const { categoryId } = params;
   const category = await getCategoryDetails(categoryId);
-
-
-  const products = [
-    {
-      id: 1,
-      name: "Product 1",
-      price: "$25.00",
-      image: "/images/product1.jpg",
-    },
-    {
-      id: 2,
-      name: "Product 2",
-      price: "$30.00",
-      image: "/images/product2.jpg",
-    },
-    {
-      id: 3,
-      name: "Product 3",
-      price: "$15.00",
-      image: "/images/product3.jpg",
-    },
-  ];
   return (
     <>
     <div className="container mx-auto py-10">
       <h2 className="text-2xl font-bold text-center mb-6">Products</h2>
-      <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 gap-6 text-center">
-
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 text-center">
         <ProductList products={category} categoryID={categoryId}/>
       </div>
     </div>
--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -1,59 +1,15 @@
 import { getCategories } from "@/actions/categoryActions";
 import CategoryList from "@/components/CategoryList";
-import Image from "next/image";
-import { ShoppingBag, Package, Tag, Home,Briefcase,Star } from "lucide-react";
 
 export default async function CategoriesPage() {
   const categories = await getCategories();
-  // const categoriesli = [
-  //   {
-  //     id: 1,
-  //     name: "Electronics",
-  //     icon: ShoppingBag,
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "Fashion",
-  //     icon: Package,
-  //   },
-  //   {
-  //     id: 3,
-  //     name: "Home Appliances",
-  //     icon: Home,
-  //   },
-  //   {
-  //     id: 4,
-  //     name: "Office Supplies",
-  //     icon: Briefcase,
-  //   },
-  //   {
-  //     id: 5,
-  //     name: "Deals & Offers",
-  //     icon: Star,
-  //   },
-  // ];
-
-
-  return (
   
+  return (
   <div className="container mx-auto py-10">
       <h2 className="text-2xl font-bold text-center mb-6">Categories</h2>
-      <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 gap-6 text-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 text-center">
       <CategoryList categories={categories} />
-        {/* {categoriesli.map((category) => (
-          <div
-            key={category.id}
-            className="flex items-center bg-white rounded-lg shadow-md p-4 hover:shadow-lg transition duration-300"
-          >
-            <div className="p-3 bg-blue-100 rounded-full">
-              <category.icon className="h-6 w-6 text-blue-600" />
-            </div>
-            <h3 className="ml-4 text-lg font-bold">{category.name}</h3>
-          </div>
-        ))} */}
       </div>
     </div>
-     
-  
   );
 }
